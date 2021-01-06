eb526f5c14a5d8a36a970bdda5d29bbbceee7f50
EC2 Default User <ec2-user@ip-172-31-73-94.ec2.internal>
2021-01-06 18:41:45 +0000

    Module 2 completed

--- AWS/main.tf
+++ AWS/main.tf
@@ -2,14 +2,6 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "la-terraform-course-state"
-    key = "terraform/terraform.tfstate"
-    region = "us-west-2"
-  }
-}
-
 # Deploy Storage Resource
 module "storage" {
   source       = "./storage"
@@ -23,4 +15,16 @@ module "networking" {
   vpc_cidr     = "${var.vpc_cidr}"
   public_cidrs = "${var.public_cidrs}"
   accessip     = "${var.accessip}"
+}
+
+# Deploy Compute Resource
+module "compute" {
+  source          = "./compute"
+  instance_count  = "${var.instance_count}"
+  key_name        = "${var.key_name}"
+  public_key_path = "${var.public_key_path}"
+  subnets         = "${module.networking.public_subnets}"
+  security_group  = "${module.networking.public_sg}"
+  subnet_ips      = "${module.networking.subnet_ips}"
+  instance_type   = "${var.server_instance_type}"
 }
--- AWS/outputs.tf
+++ AWS/outputs.tf
@@ -1,5 +1,31 @@
 #----root/outputs.tf-----
 
+#----Storage Outputs.tf-----
 output "Bucket Name" {
   value = "${module.storage.bucketname}"
 }
+
+#----Networking Outputs.tf-----
+output "Public Subnets" {
+  value = "${join(", ", module.networking.public_subnets)}"
+}
+
+output "Public Security Group" {
+  value = "${module.networking.public_sg}"
+}
+
+output "Subnet IPs" {
+  value = "${join(", ", module.networking.subnet_ips)}"
+}
+
+#----Compute Outputs.tf-----
+output "Public Instance IDs" {
+    value = "${module.compute.server_id}"
+}
+
+output "Public Instance IPs" {
+    value = "${module.compute.server_ip}"
+}
+
+
+
